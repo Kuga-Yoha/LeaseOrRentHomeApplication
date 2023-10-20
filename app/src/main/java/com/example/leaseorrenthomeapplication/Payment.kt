ed679a6b59df6379ba084ef46a4e1cae1b2c30b3
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-10-20 10:54:22 -0400

    Added Time delay and intent to homescreen

--- app/src/main/java/com/example/leaseorrenthomeapplication/Payment.kt
+++ app/src/main/java/com/example/leaseorrenthomeapplication/Payment.kt
@@ -3,6 +3,8 @@ package com.example.leaseorrenthomeapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.RadioButton
 import android.widget.RadioGroup
@@ -22,7 +24,15 @@ class Payment : AppCompatActivity() {
 
 
             if(radioBtnCash.isChecked){
-                Toast.makeText(this, "Please pay the cash when you arrive at the location to proceed", Toast.LENGTH_LONG)
+                Toast.makeText(this, "Please pay the cash when you arrive at the location to proceed", Toast.LENGTH_LONG).show()
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+
+                }, 3000)
+
+
             }else {
                 val intent = Intent(applicationContext, Checkout::class.java)
                 startActivity(intent)
